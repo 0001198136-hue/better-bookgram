2a82bc6f83145bd13d8f72f0321db62926f588de
Rodrigo Da Cruz <0001205693@senaimgaluno.com.br>
2026-05-26 13:27:22 -0300

    deploy render 2

--- start.sh
+++ start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Gera APP_KEY se não existir
+if [ -z "$APP_KEY" ]; then
+    php artisan key:generate --force
+fi
+
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
 
-php-fpm8.3 -D
+# Inicia PHP-FPM em background e Nginx em foreground
+php-fpm &
 nginx -g "daemon off;"
